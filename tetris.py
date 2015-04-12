11e6e163dbaeab4d71d1b13e57e88d6432327cfa
Martin Fredriksson <martin.fredriksson@martinfredriksson.com>
2015-04-13 01:32:55 +0200

    Added proper collisions with walls and already placed blocks

--- tetris.py
+++ tetris.py
@@ -3,8 +3,9 @@ WINDOW_TITLE = "Python Tetris "
 import pygame
 from pygame.locals import *
 import random
+from collections import Counter
 
-class TetrisBlock:
+class TetrisPart:
     #A single tetris square, i.e part of a complete tetris block
     active = True
     remove = False
@@ -13,9 +14,11 @@ class TetrisBlock:
     def __init__(self, posX, posY):
         self.pos = (posX, -40)
 
-        #self.blocking = [(0,0), (0,0), (0,0), (0,0)]
-        #Temp blocking list
         self.blocking = (0 , 0)
+        #Update immediately to make sure nothing shady happens in the first timer
+        #before update is run naturally
+        self.update()
+
     def update(self):
         #calculate what squares the active block is blocking
         #X-axis
@@ -41,9 +44,11 @@ class TetrisBlock:
     def move(self):
             self.pos = (self.pos[0], self.pos[1]+40)
 
-    def getBlocking(self, x):
-        #x=0, x-axis, x=1 y-axis
-        return self.blocking[x]
+    def getBlockingX(self):
+        return self.blocking[0]
+
+    def getBlockingY(self):
+        return self.blocking[1]
 
     def getBlockingCoord(self):
         return self.blocking[0] * self.blocking[1]
@@ -80,7 +85,7 @@ class TetrisCombined:
         len = random.randint(1, 4)
         self.parts = []
         for i in range(len):
-            self.parts.append(TetrisBlock(i*40, 20))
+            self.parts.append(TetrisPart(i*40, 20))
 
 
         #The block part texture
@@ -101,6 +106,51 @@ class TetrisCombined:
 
     #Randomize block type
     blocktype = random.randint(0, 6)
+
+    def canMoveLeftRight(self, lr, blockingSpots):
+       #left = 0, right = 1
+       if(lr == 0):
+           for part in self.parts:
+                #Check if colliding with edge, if so return false
+                if(part.blocking[0] == 1):
+                    return False
+            #Check for collissions with already placed blocks
+           for part in self.parts:
+               for blockingSpot in blockingSpots:
+                    if(part.getBlockingX()-1 == blockingSpot[0] and part.getBlockingY() == blockingSpot[1]):
+                        print "mhm yes"
+                        return False
+       elif(lr == 1):
+            for part in self.parts:
+                #Check if colliding with edge, if so return false
+                if(part.blocking[0] == 10):
+                    return False
+            #Check for collissions with already placed blocks
+            for part in self.parts:
+               for blockingSpot in blockingSpots:
+                    if(part.getBlockingX()+1 == blockingSpot[0] and part.getBlockingY() == blockingSpot[1]):
+                        return False
+       else:
+           nextPos = ""
+           print "invalid value"
+
+       #for tetris_block in self.tetris_blocks:
+       #    for part in tetris_block.getParts():
+       #        if(nextPos[0] == part.getBlocking(0) and nextPos[1] == part.getBlocking(1)):
+       #            return False
+       return True
+
+    def moveRight(self):
+        for part in self.parts:
+            #move all parts in TetrisCombined to the right
+            part.right()
+            part.update()
+    
+    def moveLeft(self):
+        for part in self.parts:
+            #move all parts in TetrisCombined to the right
+            part.left()
+            part.update()
 
     def getParts(self):
         return self.parts
@@ -138,15 +188,15 @@ class GameMain():
         for tetris_block in self.tetris_blocks:
             #print "tetris block " + str(tetris_block.getBlocking(0)) + ", " + str(tetris_block.getBlocking(1))
             for part in tetris_block.getParts():
-                if(part.getBlocking(1) == lineToRemove):
-                    print "flag to remove x:" + str(part.getBlocking(0)) + "y:" + str(part.getBlocking(1))
+                if(part.getBlockingY() == lineToRemove):
+                    print "flag to remove x:" + str(part.getBlockingX()) + "y:" + str(part.getBlockingY())
                     part.setRemoveFlag(True)
 
 #TODO THIS FUNCTION DOES NOT DELETE ALL PARTS MARKED FOR DELETION
         for tetris_block in self.tetris_blocks:
            for part in tetris_block.getParts()[::-1]:
                if(part.getRemoveFlag() == True):
-                    print "actually remove  x:" + str(part.getBlocking(0)) + "y:" + str(part.getBlocking(1))
+                    print "actually remove  x:" + str(part.getBlockingX()) + "y:" + str(part.getBlockingY())
                     tetris_block.getParts().remove(part)
 
         #Row is removed move blocks above down
@@ -155,44 +205,26 @@ class GameMain():
             moved_block = False
             for tetris_block in self.tetris_blocks:
                 for part in tetris_block.getParts():
-                    if(part.getBlocking(1) < lineToRemove):
+                    if(part.getBlockingY() < lineToRemove):
                         if(self.canMove(tetris_block)):
-                            print "moving block " + str(part.getBlocking(0)) + ", " + str(part.getBlocking(1))
+                            print "moving block " + str(part.getBlockingX()) + ", " + str(part.getBlockingY())
                             moved_block = True
                             tetris_block.move()
                             tetris_block.update()
 
 
     def checkRows(self):
-        blockedSpots = []
+        self.blockedSpots = []
         for tetris_block in self.tetris_blocks:
             for tetris_part in tetris_block.getParts():
-                blockedSpots.append(tetris_part.getBlocking(1))
+                self.blockedSpots.append((tetris_part.getBlockingX(), tetris_part.getBlockingY()))
 
-        for i in range(1,21):
-            count = blockedSpots.count(i)
-            if(count == 10):
-                self.removeLine(i)
+        # Check amount of blocks on each line and remove "full lines"
+        itemsOnLines = Counter(elem[1] for elem in self.blockedSpots)
+        for line in itemsOnLines.iteritems():
+              if(line[1] >= 10):
+                self.removeLine(line[0])
                 print "remove line"
-        #blockedSpots.sort()
-        #for b in blockedSpots:
-
-    def canMoveLeftRight(self, block, lr):
-        #left = 0, right = 0
-        if(lr == 0):
-            nextPos = (block.blocking[0] - 1, block.blocking[1])
-        elif(lr == 1):
-            nextPos = (block.blocking[0] + 1, block.blocking[1])
-        else:
-            nextPos = ""
-            print "invalid value"
-        for tetris_block in self.tetris_blocks:
-            for part in tetris_block.getParts():
-                if(nextPos[0] == part.getBlocking(0) and nextPos[1] == part.getBlocking(1)):
-                    return False
-
-        return True
-
 
 
     def canMove(self, block):
@@ -203,7 +235,7 @@ class GameMain():
                 return False
             for tetris_block in self.tetris_blocks:
                 for part in tetris_block.getParts():
-                    if(nextPos[0] == part.getBlocking(0) and nextPos[1] == part.getBlocking(1)):
+                    if(nextPos[0] == part.getBlockingX() and nextPos[1] == part.getBlockingY()):
                         return False
 
         return True
@@ -230,10 +262,12 @@ class GameMain():
 
         #Load tetris song
         pygame.mixer.music.load("tetris.ogg")
-        pygame.mixer.music.play(loops=-1)
+        #pygame.mixer.music.play() #loops=-1
 
         self.tetris_blocks = []
         self.active_block = TetrisCombined()
+
+        self.blockedSpots = []
 
 
         # save w, h, and screen
@@ -300,17 +334,23 @@ class GameMain():
                 if event.key == K_LEFT:
                     #Move block left
                     print "left pressed"
-                    for part in self.active_block.getParts():
-                        if(part.blocking[0] > 1 and self.canMoveLeftRight(part, 0)):
-                            part.left()
-                            part.update()
+                    if(self.active_block.canMoveLeftRight(0,self.blockedSpots)):
+                        self.active_block.moveLeft()
+                    #for part in self.active_block.getParts():
+                    #    if(part.blocking[0] > 1 and self.canMoveLeftRight(part, 0)):
+                    #        part.left()
+                    #        part.update()
                 if event.key == K_RIGHT:
                     #Move block right
                     print "right pressed"
-                    for part in self.active_block.getParts():
-                        if(part.blocking[0] < 10 and self.canMoveLeftRight(part, 1)):
-                            part.right()
-                            part.update()
+                    if(self.active_block.canMoveLeftRight(1,self.blockedSpots)):
+                        self.active_block.moveRight()
+
+                    #for part in self.active_block.getParts():
+                    #    if(part.blocking[0] < 10 and self.canMoveLeftRight(part, 1)):
+                    #        part.right()
+                    #        part.update()
+
                 if event.key == K_SPACE:
                     print "drop pressed"
                     #self.active_block.drop(self)
